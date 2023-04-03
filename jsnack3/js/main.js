df237d80fc868665ef6896b046327f2613497b95
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-03 16:44:07 +0200

    JSnack3 completo.

---
+++ jsnack3/js/main.js
@@ -0,0 +1,12 @@
+/Il software deve chiedere per 10 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti./
+
+let inputNumbers = [];
+for (let i = 0; i < 10; i++) {
+    inputNumbers.push(+prompt("Scrivi un numero"));
+}
+let sumOfNumbers = 0;
+
+inputNumbers.map(x => sumOfNumbers += x);
+
+console.log(sumOfNumbers);
+
