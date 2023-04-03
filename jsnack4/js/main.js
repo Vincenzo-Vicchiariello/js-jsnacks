62c938d70c0a2fe31e4e2f57aee258cd65daa04b
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-03 17:19:13 +0200

    .

--- jsnack4/js/main.js
+++ jsnack4/js/main.js
@@ -1,5 +1,4 @@
-* /In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.*/
-
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.//
 
 let gatsbySGuests = ["Tizio", "Caio", "Sempronio", "Ambrogio", "Giammaria", "Adolf Hitler", "Francisco Franco", "Ted Kaczynski"];
 
