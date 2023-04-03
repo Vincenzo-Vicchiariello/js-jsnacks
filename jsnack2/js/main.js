66d07a2d8242096a6b7ed9db0ddde30cde468016
Vincenzo-Vicchiariello <v.vicchiariello@outlook.it>
2023-04-03 15:46:27 +0200

    Aggiunta consegna anche a jsnack2.

--- jsnack2/js/main.js
+++ jsnack2/js/main.js
@@ -1,3 +1,6 @@
+/*L’utente inserisce due parole in successione, con due prompt.
+Il software stampa prima la parola più corta, poi la parola più lunga.*/
+
 let parola1 = prompt("inserici la prima parola");
 let length1 = parola1.length;
 let parola2 = prompt("inserici la seconda parola");
